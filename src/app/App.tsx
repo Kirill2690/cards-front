57233751493e181a98ddc76d732cf7af35b6262b
Kirill2690 <kirillmuravev2690@gmail.com>
2022-11-12 11:52:08 +0300

    refactor: index.tsx,App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Provider} from "react-redux";
-import {store} from "../bll/store";
-import {HashRouter} from "react-router-dom";
 import {Main} from "./Main";
 
 const App = () => (
     <div className="App">
-      <Provider store={store}>
-        <HashRouter>
-          <Main/>
-        </HashRouter>
-      </Provider>
+        <Main/>
     </div>
 );
 
