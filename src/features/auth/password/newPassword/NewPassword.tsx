5212fa38f627eccf60f47aeb38bfa3c8ee3e3747
Kirill2690 <kirillmuravev2690@gmail.com>
2022-11-15 01:58:50 +0300

    fixed:components:NewPassword

--- src/features/auth/password/newPassword/NewPassword.tsx
+++ src/features/auth/password/newPassword/NewPassword.tsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './NewPassword.module.css'
+import {Navigate, useNavigate, useParams} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../../common/hooks/hooks";
+import {AppRootStateType} from "../../../../app/store";
+import {useFormik} from "formik";
+import {setNewPasswordTC} from "./newPassword-reducer";
+import {Button, FormControl, IconButton, Input, InputAdornment, LinearProgress, Paper} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 
+type FormikErrorType = {
+    password?: string
+}
 export const NewPassword = () => {
-    return (
-        <div className={s.wrapper_password}>
-            Enter new password
-        </div>
-    );
-};
+    const navigate = useNavigate()
+
+const dispatch = useAppDispatch()
+const isPassChanged = useAppSelector((state: AppRootStateType) => state.newPassword.isPassChanged)
+const appStatus = useAppSelector(state => state.app.status)
+
+const params = useParams()
+const token = params.token
+
+
+const [showPassword, setShowPassword] = useState(false)
+const onClickShowPassword = () => setShowPassword(!showPassword)
+
+
+const formik = useFormik({
+    initialValues: {
+        password: '',
+        resetPasswordToken: token,
+    },
+    validate: (values) => {
+        const errors: FormikErrorType = {}
+        if (values.password.length < 8) {
+            errors.password = 'enter more than 7 symbols'
+        }
+        return errors
+    },
+    onSubmit: values => {
+        dispatch(setNewPasswordTC(values))
+        formik.resetForm()
+    }
+})
+if (isPassChanged) {
+    return <Navigate to={'/login'}/>
+}
+return (
+    <div className={s.wrapper_newPassword}>
+        <h2 className={s.title}>Create new password</h2>
+        <form className={s.form} onSubmit={formik.handleSubmit}>
+            <div className={s.inputForm}>
+                <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    {...formik.getFieldProps('password')}
+                    placeholder='Password'
+                    className={s.input}
+                    color={'primary'}
+                />
+                <IconButton className={s.icon} onClick={onClickShowPassword}>
+                    {showPassword ? <VisibilityOff/> : <Visibility/>}
+                </IconButton>
+                <div className={s.error}>
+                    {formik.touched.password && formik.errors.password && formik.errors.password}
+                </div>
+            </div>
+            <div className={s.text}>
+                Create new password and we will send you further instructions to email
+            </div>
+            <div className={s.buttonForm}>
+                <Button onClick={() => navigate('/login')}
+                        className={s.button}
+                        variant={'contained'}>
+                    Create new password</Button>
+            </div>
+        </form>
+    </div>
+)
+}
+
--- src/features/auth/password/newPassword/newPassword-reducer.ts
+++ src/features/auth/password/newPassword/newPassword-reducer.ts
@@ -1,10 +1,52 @@
-const initialState = {}
+import {setAppStatusAC} from "../../../../app/app-reducer";
+import {AxiosError} from "axios";
+import {errorUtil} from "../../../../common/utils/utils-error";
+import {AppThunk} from "../../../../app/store";
+import {authAPI, SetNewPasswordType} from "../../../../api/api";
 
-export const newPasswordReducer = (state: any = initialState, action: any) => {
+
+const initialState: InitialStateType = {
+    info: '',
+    isPassChanged: false
+}
+
+export const newPasswordReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case '':
-            return state
+        case 'NEW-PASSWORD-SUCCESS':
+            return {...state, info: action.info}
+        case 'IS-PASS-CHANGED':
+            return {...state, isPassChanged: action.isPassChanged}
         default:
             return state
     }
 }
+
+// thunks
+export const setNewPasswordTC = (data: SetNewPasswordType): AppThunk => {
+    return (dispatch) => {
+        dispatch(setAppStatusAC('loading'))
+        authAPI.setNewPassword(data)
+            .then((res) => {
+                dispatch(setInfoAC(res.data.info))
+                dispatch(setPassChangedAC(true))
+            })
+            .catch((error: AxiosError<{ error: string }>) => {
+                errorUtil(error, dispatch)
+            })
+            .finally(() => {
+                dispatch(setAppStatusAC('succeeded'))
+            })
+    }
+}
+
+// actions
+export const setInfoAC = (info: string) => ({type: 'NEW-PASSWORD-SUCCESS', info} as const)
+export const setPassChangedAC = (isPassChanged: boolean) => ({type: 'IS-PASS-CHANGED', isPassChanged} as const)
+
+// types
+type InitialStateType = {
+    info: string
+    isPassChanged: boolean
+}
+
+type ActionsType = ReturnType<typeof setInfoAC> | ReturnType<typeof setPassChangedAC>
