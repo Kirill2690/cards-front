8d80b39447df63ed5ff535ee02a0870d4397179f
Zhanet <zhanet_f@mail.ru>
2022-11-16 00:06:11 +0300

    feat:ErrorPage and styles

--- src/common/components/error404/Error404.tsx
+++ src/common/components/error404/Error404.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Error404.module.css'
-import error404 from  '../../../assets/images/error_404.png'
+import {Navigate} from "react-router-dom";
+import pageNotFound from "../../../assets/images/—Pngtree—404 error with a broken_7222901.png"
+import {Button} from "@mui/material";
 
 export const Error404 = () => {
+    const [error, setError] = useState<boolean>(false)
+
+    const onClickHandler = () => {
+        setError(true)
+    }
+
+    if (error) {
+        return <Navigate to={'/login'}/>
+    }
     return (
         <div className={s.error404}>
-            <img className={s.img_error} src={error404} alt={'error'}/>
+
+            <div>
+                <img className={s.img_error} src={pageNotFound} alt={'error'}/>
+            </div>
+            <div className={s.error_btn}>
+                <Button onClick={onClickHandler} style={{color:'#F9F9FA'}}>↩ Back to homepage</Button>
+            </div>
         </div>
     );
 };
