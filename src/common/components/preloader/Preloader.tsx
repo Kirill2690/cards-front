9b685cad035c38ff9b4174d1c1988c746d146c5c
Kirill2690 <kirillmuravev2690@gmail.com>
2022-11-18 01:47:44 +0300

    fixed:Preloader

--- src/common/components/preloader/Preloader.tsx
+++ src/common/components/preloader/Preloader.tsx
@@ -1,10 +1,10 @@
-import {LinearProgress} from "@mui/material";
+import {CircularProgress} from "@mui/material";
 
 export const Preloader = () => {
     return (
         <div
             style={{position: 'fixed', top: '45%', textAlign: 'center', width: '100%', height: '100vh'}}>
-            <LinearProgress color={'inherit'}/>
+            <CircularProgress color={'inherit'}/>
         </div>
     );
 };
