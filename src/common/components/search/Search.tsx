b7d9499958bf3b8ef5bec9b1894a10e29c548096
MKotliarenko <kotliarenkom282@gmail.com>
2022-11-23 20:26:15 +0100

    add:Slider,Search,debounce

--- src/common/components/search/Search.tsx
+++ src/common/components/search/Search.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import {FormControl, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
-import {useAppDispatch} from "../../hooks/hooks";
+import {useDebounce} from "../../hooks/debounce";
 
 
-export const Search = (props:SearchPropsType) => {
+export const Search = (props: SearchPropsType) => {
 
-    const dispatch = useAppDispatch()
+    const [text, setText] = React.useState<string>("");
+
+    const debouncedText = useDebounce(text);
+    props.handleChangeSearch(debouncedText)
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-            props.handleChangeText (event.target.value);
-        };
+        setText (event.target.value);
+    };
 
     return (
         <div>
-            <FormControl sx={{m: 1, height:36}} variant="outlined" size="small">
+            <FormControl sx={{m: 1, height: 36}} variant="outlined" size="small">
                 <InputLabel htmlFor="search"> Provide your text </InputLabel>
                 <OutlinedInput
                     id="search"
                     type='text'
-                    value={props.value}
+                    value={text}
                     onChange={handleChange}
                     endAdornment={
                         <InputAdornment position="end">
-                                <SearchIcon/>
+                            <SearchIcon/>
                         </InputAdornment>
                     }
                     label="Provide your text"
@@ -34,6 +37,6 @@ export const Search = (props:SearchPropsType) => {
 };
 
 type SearchPropsType = {
-    handleChangeText: (value: string) => void
-    value:string
+    handleChangeSearch: (value:string) => void
 }
+
--- src/common/components/slider/NewSlider.tsx
+++ src/common/components/slider/NewSlider.tsx
@@ -1,47 +1,57 @@
-import React from 'react';
+import React, {SyntheticEvent} from 'react';
 import Slider from '@mui/material/Slider';
 import s from './NewSlider.module.css'
+import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
+import {setQueryParamsAC} from "../../../features/packs/packs-reducer";
 
 
 const minDistance = 1;
 
-export const NewSlider = (props: NewSliderPropsType) => {
-    const [value, setValue] = React.useState<number[]>([props.minValue, props.maxValue]);
+export const NewSlider = () => {
 
-    const handleChange = (
-        event: Event,
-        newValue: number | number[],
-        activeThumb: number,
-    ) => {
+    const minCards = useAppSelector(state => state.packs.minCardsCount)
+    const maxCards = useAppSelector(state => state.packs.maxCardsCount)
+
+    const dispatch = useAppDispatch()
+
+    const [value, setValue] = React.useState<number[]>([minCards, maxCards]);
+
+    const handleChange = (event: Event, newValue: number | number[], activeThumb: number,) => {
         if (!Array.isArray(newValue)) {
             return;
         }
-
-        if (activeThumb === 0) {
-            setValue([Math.min(newValue[0], value[1] - minDistance), value[1]]);
-        } else {
-            setValue([value[0], Math.max(newValue[1], value[0] + minDistance)]);
-        }
+        activeThumb === 0
+            ?
+            setValue([Math.min(newValue[0], value[1] - minDistance), value[1]])
+            :
+            setValue([value[0], Math.max(newValue[1], value[0] + minDistance)])
     };
+
+    const handleChangeCommitted = (event: Event | SyntheticEvent<Element, Event>,
+                                   newValue: number | number[]) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+        dispatch(setQueryParamsAC({min: newValue[0].toString(), max: newValue[1].toString()}))
+    }
+
     return (
         <div className={s.wrapper}>
-            <div className={s.span}>{value[0]}</div>
-            <Slider
-                max={props.maxValue}
-                min={props.minValue}
-                getAriaLabel={() => 'Minimum distance'}
-                value={value}
-                onChange={handleChange}
-                valueLabelDisplay="auto"
-                disableSwap
-            />
-
-            <div className={s.span}>{value[1]}</div>
+            <div className={s.minMaxBox}>{value[0]}</div>
+            <div className={s.slider}>
+                <Slider
+                    size={'small'}
+                    min={minCards}
+                    max={maxCards}
+                    getAriaLabel={() => 'Minimum distance'}
+                    value={value}
+                    onChangeCommitted={handleChangeCommitted}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    disableSwap
+                />
+            </div>
+            <div className={s.minMaxBox}>{value[1]}</div>
         </div>
     );
 };
-
-type NewSliderPropsType = {
-    minValue: number,
-    maxValue: number
-}
